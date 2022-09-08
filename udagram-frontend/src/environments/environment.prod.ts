85bdfdc85a93dfdda716442abfef4c9926364522
mengestu <binwelbeck@gmail.com>
2022-09-08 06:07:22 +0300

    added the deplyment for kubernates

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad63f6e0da58b4e27b5b142f30d9cf9d-2111450160.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
